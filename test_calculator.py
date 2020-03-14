9693c1654a7c8df716abe69bab91252ef00a0c41
nphute <nndphute@gmail.com>
2020-03-14 20:56:58 +0530

    Add all

---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+"""
+Unit tests for the calculator library
+"""
+
+import calculator
+
+class TestCalculator():
+
+
+    def test_add(self):
+        assert 4 == calculator.add(2,2)
+
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(4,2)
